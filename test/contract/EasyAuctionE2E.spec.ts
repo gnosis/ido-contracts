249b6e14deff63fc87bfc633d9cbbd5985e00977
josojo <josojo@hotmail.de>
2021-01-26 14:46:24 +0100

    adding tests for benchmarking preSum

--- test/contract/EasyAuctionE2E.spec.ts
+++ test/contract/EasyAuctionE2E.spec.ts
@@ -60,4 +60,49 @@ describe("EasyAuction", async () => {
 
     console.log("Gas usage for verification", gasUsed.toString());
   });
+  it("e2e - to test the gas consumed by precalculateSellAmountSum", async () => {
+    const {
+      auctioningToken,
+      biddingToken,
+    } = await createTokensAndMintAndApprove(easyAuction, [user_1, user_2], hre);
+    const nrTests = 100; // increase here for better gas estimations, nrTests-2 must be a divisor of 10**18
+    const auctionId: BigNumber = await sendTxAndGetReturnValue(
+      easyAuction,
+      "initiateAuction(address,address,uint256,uint256,uint96,uint96,uint256,uint256,bool)",
+      auctioningToken.address,
+      biddingToken.address,
+      60 * 60,
+      60 * 60,
+      ethers.utils.parseEther("1000"),
+      ethers.utils.parseEther("1000"),
+      1,
+      0,
+      false,
+    );
+
+    for (let i = 2; i < nrTests; i++) {
+      const sellOrder = [
+        {
+          sellAmount: ethers.utils
+            .parseEther("1000")
+            .div(BigNumber.from(nrTests - 2)),
+          buyAmount: BigNumber.from("10")
+            .pow(BigNumber.from(18))
+            .mul(1000)
+            .div(BigNumber.from(nrTests - 2))
+            .mul(i - 1)
+            .div(BigNumber.from(i)),
+          userId: BigNumber.from(1),
+        },
+      ];
+      await placeOrders(easyAuction, sellOrder, auctionId, hre);
+    }
+    await closeAuction(easyAuction, auctionId);
+    const tx = await easyAuction.precalculateSellAmountSum(
+      auctionId,
+      nrTests - 3,
+    );
+    const gasUsed = (await tx.wait()).gasUsed;
+    console.log("Gas usage for verification", gasUsed.toString());
+  });
 });
