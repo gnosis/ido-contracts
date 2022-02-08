293c04ce2bb87e601ea85abda30ded61bf23b520
josojo <josojo@hotmail.de>
2022-02-08 18:58:05 +0100

    updating gnosis chain rpc node

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -104,7 +104,7 @@ export default {
     },
     xdai: {
       ...sharedNetworkConfig,
-      url: "https://xdai.1hive.org",
+      url: "https://rpc.gnosischain.com/",
       gasPrice: GAS_PRICE_GWEI
         ? parseInt(
             utils.parseUnits(GAS_PRICE_GWEI.toString(), "gwei").toString(),
